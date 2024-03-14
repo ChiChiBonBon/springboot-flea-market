e2b326b4197de8d017d9ff08cb5aad8756073ee0
ChiBon <73291132+ChiChiBonBon@users.noreply.github.com>
2024-03-15 00:28:18 +0800

    「商品功能 - 實作查詢商品列表(基礎實作)」

--- flea-market/src/main/java/com/chibon/fleamarket/dao/ProductDao.java
+++ flea-market/src/main/java/com/chibon/fleamarket/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.chibon.fleamarket.dao;
 import com.chibon.fleamarket.dto.ProductRequest;
 import com.chibon.fleamarket.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- flea-market/src/main/java/com/chibon/fleamarket/service/ProductService.java
+++ flea-market/src/main/java/com/chibon/fleamarket/service/ProductService.java
@@ -3,7 +3,11 @@ package com.chibon.fleamarket.service;
 import com.chibon.fleamarket.dto.ProductRequest;
 import com.chibon.fleamarket.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
