c0eee612dc80b568c5bea503229a5527aefcd2c1
ChiBon <73291132+ChiChiBonBon@users.noreply.github.com>
2024-03-23 17:39:11 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- flea-market/src/main/java/com/chibon/fleamarket/service/impl/UserServiceImpl.java
+++ flea-market/src/main/java/com/chibon/fleamarket/service/impl/UserServiceImpl.java
@@ -4,11 +4,18 @@ import com.chibon.fleamarket.dao.UserDao;
 import com.chibon.fleamarket.dto.UserRegisterRequest;
 import com.chibon.fleamarket.model.User;
 import com.chibon.fleamarket.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserDao userDao;
 
@@ -19,6 +26,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查註冊的email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            log.warn("該email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
