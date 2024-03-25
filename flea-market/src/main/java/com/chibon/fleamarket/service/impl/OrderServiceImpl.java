bca89bfb3dfe3af71dc076e2a20741190b19bae9
ChiBon <73291132+ChiChiBonBon@users.noreply.github.com>
2024-03-26 01:06:14 +0800

    訂單功能 - 檢查user是否存在、扣除商品庫存

--- flea-market/src/main/java/com/chibon/fleamarket/service/impl/OrderServiceImpl.java
+++ flea-market/src/main/java/com/chibon/fleamarket/service/impl/OrderServiceImpl.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Autowired
     private OrderDao orderDao;
@@ -30,15 +31,38 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private ProductDao productDao;
 
+    @Autowired
+    private UserDao userDao;
+
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        //檢查user是否存在
+        User user = userDao.getUserById(userId);
+
+        if(user == null){
+            log.warn("該 userId {} 不存在",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            //檢查Product 是否存在、庫存是否足夠
+            if(product == null){
+                log.warn("商品 {} 不存在",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } else if (product.getStock() < buyItem.getQuantity()){
+                log.warn("商品 {} 庫存數量不足，無法購買。剩餘庫存 {}，欲購買數量 {}",
+                        buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            //扣除商品庫存
+            productDao.updateStock(product.getProductId(),product.getStock() - buyItem.getQuantity());
 
             //計算總價錢
             int amount = buyItem.getQuantity() * product.getPrice();
